8f19763ba116a19d062c126d61726da41a03c90a
Hasibul294 <hasibulhasan294@gmail.com>
2024-03-18 16:01:12 +0600

    first setup and so many things done

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { HiOutlineHashtag, HiOutlineHome, HiOutlineMenu, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
+import { RiCloseLine } from 'react-icons/ri';
+import { IoMdTrendingUp } from 'react-icons/io';
+import { FaHandPointRight } from 'react-icons/fa';
+
+import { logo } from '../assets';
+
+const links = [
+  { name: 'Home', to: '/', icon: HiOutlineHome },
+  { name: 'Trending Now', to: '/trending-now', icon: IoMdTrendingUp },
+  { name: 'Recommendation', to: '/recommendation', icon: FaHandPointRight },
+  { name: 'Top Artist', to: '/top-artists', icon: HiOutlineUserGroup },
+];
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    // bg-[#191624]
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-gradient-color-service">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+
+      {/* Mobile sidebar */}
+      <div className="absolute md:hidden block top-6 right-3">
+        {!mobileMenuOpen ? (
+          <HiOutlineMenu className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(true)} />
+        ) : (
+          <RiCloseLine className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(false)} />
+        )}
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
